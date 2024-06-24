3a2b4d55b403cdbe6cab5da7949b1386ce256bec
coliin8 <jakecollins1976@gmail.com>
2024-06-24 14:15:57 +0100

    added permission into ui search view

--- invenio_employee_profiles/views/employee_profiles.py
+++ invenio_employee_profiles/views/employee_profiles.py
@@ -1,9 +1,12 @@
-from flask import current_app, g, render_template
+from flask import g, render_template
 
+from ..proxies import current_profiles
 
 def employee_profiles_search():
     """Employee Profiles search page."""
+    can_create = current_profiles.service.check_permission(g.identity, "create")
     return render_template(
         # This seems to point to invenio_employee_profiles/templates/semantic-ui/invenio_employee_profiles/search.html
         "invenio_employee_profiles/search.html",
+        permissions=dict(can_create=can_create),
     )
